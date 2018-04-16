62904a28b4e46887dabc82192194fc43871d1562
rmsare <robertmsare@gmail.com>
2018-04-16 11:57:54 -0700

    Import intersects

--- data_utils.py
+++ data_utils.py
@@ -6,6 +6,7 @@ import matplotlib
 import rasterio.features
 
 from shapely.geometry import shape
+from spatail_utils import intersects
 
 import matplotlib.pyplot as plt
 
@@ -14,26 +15,6 @@ from copy import copy
 from progressbar import ETA, Bar, Percentage, ProgressBar
 
 
-def intersects(feature, dataset):
-    """
-    Returns True if vector feature and raster bounding boxes intersect.
-    """
-
-    feature_left, feature_bottom, feature_right, feature_top = rasterio.features.bounds(feature)
-    raster_left, raster_bottom, raster_right, raster_top = dataset.bounds
-
-    intersect_bottom = feature_bottom <= raster_top and feature_bottom >= raster_bottom
-    intersect_top = feature_top >= raster_bottom and feature_top <= raster_top
-    intersect_left = feature_left <= raster_right and feature_left >= raster_left
-    intersect_right = feature_right >= raster_left and feature_right <= raster_right
-
-    intersect_x = intersect_left or intersect_right
-    intersect_y = intersect_bottom or intersect_top
-
-    if intersect_x and intersect_y:
-        return True
-
-    return False
 
 def make_chip(feature, raster):
     """
@@ -196,8 +177,8 @@ def save_labelled_chips(path, out_path, label_values=None):
 
 def main(data_file, chips_path):
 
-#    print('splitting raster...')
-#    divide_raster(data_file, size=256, directory=chips_path)
+    print('splitting raster...')
+    divide_raster(data_file, size=256, directory=chips_path)
 
     print('saving images...')
     files = os.listdir(chips_path)
